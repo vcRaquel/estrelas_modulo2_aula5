c3138d9f7282662bcaaf442a863637ef46cad97d
Raquel Leão <vc.raquel@gmail.com>
2021-08-31 15:30:24 -0300

    exercicio 2

---
+++ src/br/com/zup/Exercicio2.java
@@ -0,0 +1,23 @@
+package br.com.zup;
+
+import java.util.Scanner;
+
+public class Exercicio2 {
+    public static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in);
+        int contador = 1;
+        int quantidadeDeNumeros;
+        int soma =0;
+
+
+        System.out.println("Digite a quantidade de números inteiros: ");
+        quantidadeDeNumeros = leitor.nextInt();
+
+        for (contador=1; contador <= quantidadeDeNumeros; contador += 1){
+            System.out.println("escreva um número: ");
+            soma += leitor.nextInt();
+        }
+        double media = soma/quantidadeDeNumeros;
+        System.out.println("a média dos números digitados é: " + media);
+    }
+}
