5a8bf9e992ac8661172cd22a60b746fa7998c4d8
Raquel Leão <vc.raquel@gmail.com>
2021-08-31 16:15:57 -0300

    exercicio 5

---
+++ src/br/com/zup/Exercicio5.java
@@ -0,0 +1,12 @@
+package br.com.zup;
+
+public class Exercicio5 {
+    public static void main(String[] args) {
+        int inteiro =0;
+
+        for (inteiro =0;inteiro <= 100000 ; inteiro += 1000){
+            System.out.println(inteiro);
+        }
+
+    }
+}
